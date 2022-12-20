8764b121ad3f13f5ad1db4f9a9c359ee032105b3
MatthiasBlc <mat.bouloc@gmail.com>
2022-12-20 17:23:44 +0100

    password generator and basics tests

---
+++ test/password_generator_test.exs
@@ -0,0 +1,87 @@
+defmodule PasswordGeneratorTest do
+  use ExUnit.Case
+  # doctest PasswordGenerator
+
+  # Initials tests
+  setup do
+    options = %{
+      "length" => "10",
+      "numbers" => "false",
+      "uppercase" => "false",
+      "symbols" => "false"
+    }
+
+    options_type = %{
+      lowercase: Enum.map(?a..?z, &<<&1>>),
+      numbers: Enum.map(0..9, &Integer.to_string(&1)),
+      uppercase: Enum.map(?A..?Z, &<<&1>>),
+      symbols: String.split("!#$%&()*+-./:;<=>?@[]^_{|}~", "", trim: true)
+    }
+
+    {:ok, result} = PasswordGenerator.generate(options)
+
+    %{
+      options_type: options_type,
+      result: result
+    }
+  end
+
+  test "return a string", %{result: result} do
+    assert is_bitstring(result)
+  end
+
+  test "return error when no length is given" do
+    options = %{"invalid" => "false"}
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
+
+  test "return error when length is not an integer" do
+    options = %{"length" => "ab"}
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
+
+  test "length of returned string is the option provided" do
+    length_option = %{"length" => "5"}
+    {:ok, result} = PasswordGenerator.generate(length_option)
+
+    assert 5 = String.length(result)
+  end
+
+  test "return a lowercase string just with the length", %{options_type: options} do
+    length_option = %{"length" => "5"}
+    {:ok, result} = PasswordGenerator.generate(length_option)
+
+    assert String.contains?(result, options.lowercase)
+
+    refute String.contains?(result, options.numbers)
+    refute String.contains?(result, options.uppercase)
+    refute String.contains?(result, options.symbols)
+  end
+
+  test "return error when options values are not booleans" do
+    options = %{
+      "length" => "10",
+      "numbers" => "invalid",
+      "uppercase" => "0",
+      "symbols" => "false"
+    }
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
+
+  test "return error when options not allowed" do
+    options = %{"length" => "5", "invalid" => "true"}
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
+
+  test "return error with 1 option not allowed" do
+    options = %{"length" => "5", "numbers" => "true", "invalid" => "true"}
+
+    assert {:error, _error} = PasswordGenerator.generate(options)
+  end
+
+  # Added tests
+end
